116b6b2627c7bbcd36efcf2af488f5fb84f437b7
famiri60 <127374613+famiri60@users.noreply.github.com>
2023-08-21 20:49:51 -0700

    Add files via upload

---
+++ DS_Salary_prediction/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from predict import estimation_page
+from explore import explore_page
+from predict import add_bg_from_local
+
+page = st.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
+
+
+
+
+if page=="Predict":
+    estimation_page()
+    add_bg_from_local('background.jpg')
+else:
+    explore_page()
+    
